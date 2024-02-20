7f5cf5a898b8355e66fc59b6930e27ae7b06232a
STARRY-S <hxstarrys@gmail.com>
2024-02-20 16:00:35 +0800

    Update gcp scripts

--- scripts/gcp/apply-kvm.sh
+++ scripts/gcp/apply-kvm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-gcloud compute instances update-from-file starry-ubuntu-worker \
+gcloud compute instances update-from-file starry-kvm-2 \
     --source=starry-1.yaml \
     --most-disruptive-allowed-action=RESTART \
     --zone=asia-northeast1-b
--- scripts/gcp/export-kvm.sh
+++ scripts/gcp/export-kvm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
 
-gcloud compute instances export starry-ubuntu-worker \
+gcloud compute instances export starry-kvm-2 \
   --destination=starry-1.yaml \
   --zone=asia-northeast1-b
